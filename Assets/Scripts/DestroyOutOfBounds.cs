735c608f9bd7acb1bb99567efdcf6b39e1168071
ARehman212 <abdulrehmanarm3975@gmail.com>
2024-08-31 22:39:40 +0500

    3 update

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBoundry = 33f;
+    private float lowerBoundry = -10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z > topBoundry)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBoundry)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
